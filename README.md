5968d1c9c06c348bfa810f931b667afb8ff83517
1jss <62594271+1jss@users.noreply.github.com>
2024-04-13 19:30:09 +0200

    Added the library, example and README

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # QuickLoad
- A tiny HTMX inspired content loader
+QuickLoad.js is a tiny JS library heavily inspired by HTMX that lets the user build dynamic web pages with minimal effort.
+ 
+ Example:
+ ```HTML
+<!-- This will load the content of "page.html" into the element with id "target" when the div is clicked. -->
+<div ql-get="page.html" ql-event="click" ql-target="#target"></div>
+
+ <!-- This will, when loaded, replace the element with id "target" with the content of "page.html" -->
+ <div ql-get="page.html" ql-event="load" ql-target="#target" ql-swap="outerHTML"></div>
+
+<!-- This will load the content of "page.html" into the element itself when the div is clicked -->
+ <div ql-get="page.html"></div>
+ ```
+
+The library works by finding all elements on the page that have the HTML attribute "ql-get". This is the only attribute that is required for the library to work.
+ 
+Supported attributes:
+- **ql-get**: The URL of the page to load content from. Only GET requests are supported.
+- **ql-event**: The event that triggers the content load. Can be either "click" or "load".
+- **ql-target**: The target element to load the content into. If not specified, the content will be loaded into the element itself.
+- **ql-swap**: The element swapping mode. Can either be innerHTML or outerHTML. If not specified, the default mode is innerHTML.
+ 
---
+++ quickload.js
@@ -0,0 +1,66 @@
+/**
+ * @param {HTMLElement} parentElement
+ * @returns {Array<HTMLElement>}
+ */
+function findElements(parentElement) {
+  var elements = parentElement.querySelectorAll("[ql-get]");
+  return elements
+}
+
+/**
+ * @param {HTMLElement} element
+ * @returns {void}
+ */
+function addEvent(element) {
+  var event = element.getAttribute('ql-event');
+  if (event === 'load') {
+    window.addEventListener('load', function () {
+      getContent(element);
+    });
+  } else {
+    element.addEventListener('click', function () {
+      getContent(element);
+    });
+  }
+}
+
+/**
+ * @param {HTMLElement} element
+ * @returns {void}
+ */
+function getContent(element) {
+  var url = element.getAttribute('ql-get');
+  var target = element.getAttribute('ql-target');
+  var targetElement = target ? document.querySelector(target): element;
+  if(!targetElement) return; // Target wanted, but not found
+  var swap = element.getAttribute('ql-swap') || 'innerHTML';
+  var request = new XMLHttpRequest();
+  request.open('GET', url, true);
+  request.onreadystatechange = function () {
+    if (request.readyState === 4 && request.status === 200) {
+      if (swap === 'outerHTML' && targetElement.tagName !== 'BODY') {
+        var tempElement = document.createElement('div')
+        tempElement.innerHTML = request.responseText;
+        findElements(tempElement).forEach(element => {
+          addEvent(element);
+        });
+        targetElement.parentNode.replaceChild(tempElement.firstChild, targetElement);
+      } else {
+        targetElement.innerHTML = request.responseText;
+        findElements(targetElement).forEach(element => {
+          addEvent(element);
+        });
+      }
+    }
+  };
+  request.send();
+}
+
+// Initialize the library
+(function () {
+  var elements = findElements(document);
+  if (!elements.length) return;
+  elements.forEach(element => {
+    addEvent(element);
+  })
+})();
